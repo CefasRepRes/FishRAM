9446a340caf89b587ed1c6ae6a1611c4303bc549
Marieke Desender (Cefas) <marieke.desender@cefas.gov.uk>
2023-01-27 14:32:01 +0000

    WC and WR comes from the params file not from the states file. changed states to params

--- R/simulation_functions.R
+++ R/simulation_functions.R
@@ -110,12 +110,12 @@ step = function(sim, t){
   catch_limit_commercial <- states$CLim * states$CAllocCom#individuals
   catch_limit_recreation <- states$CLim * states$CAllocRec#individuals
 
-  excessCatchCommercial <- (states$LC/states$WC) - catch_limit_commercial#individuals
-  excessCatchRecreation <- (states$LR/states$WR) - catch_limit_recreation#individuals
+  excessCatchCommercial <- (states$LC/params$WC) - catch_limit_commercial#individuals
+  excessCatchRecreation <- (states$LR/params$WR) - catch_limit_recreation#individuals
 
   if(excessCatchCommercial > 0){
     #If you caught too many fish, you didnt land or sell them, so recalculate how much you landed and how much profit you made.
-    states$LC <- states$LC - (excessCatchCommercial* states$WC)#weights
+    states$LC <- states$LC - (excessCatchCommercial* params$WC)#weights
     states$tau <- params@nu * states$LC - (params@baromega * states$EC + params@Lambda * params@phi)
     states$VC  <- params@sigma * params@nu * states$LC
     #Also put them back in the water, with only some surviving
@@ -124,7 +124,7 @@ step = function(sim, t){
 
   #Now do the same for the recreational fleet
   if(excessCatchRecreation > 0){
-    states$LR <- states$LR - (excessCatchRecreation*states$WR)#weights
+    states$LR <- states$LR - (excessCatchRecreation*params$WR)#weights
     states$SA <- states$SA + (1 - params@varphi)*excessCatchRecreation#individuals
   }
   return(states)
